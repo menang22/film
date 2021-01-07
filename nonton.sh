7ff008bab00db8fcfb25e9d5ecf4de210fbd43da
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-07 19:53:25 +0700

    Create nonton.sh

---
+++ nonton.sh
@@ -0,0 +1,14 @@
+#!system/bin/sh
+import os, sys, time, date
+from time import sleep
+echo selamat datang di menu nonton film starwars menang22
+echo nonton dimulai
+echo silahkan install bahan terlebih dahulu
+os.system(sleep 1)
+apt update && apt upgrade
+apt install python python2
+apt install git
+apt install curl
+os.system(sleep 2)
+apt install busybox
+os.system(sleep 3)
