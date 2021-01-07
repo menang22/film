d4bd86ff9cc01220888d70d3030e9d22be030359
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-07 22:24:21 +0700

    Update nonton.sh

--- nonton.sh
+++ nonton.sh
@@ -1,13 +1,13 @@
 #!system/bin/sh
 import os, sys, time, date
 from time import sleep
-red=\033[31:1m
-blue=\033[30:1m
-green=\033[33:1m
+red=\033[31;1m'
+blue=\033[30;1m'
+green=\033[33;1m'
 python pw.py
-echo \033[33:1mselamat datang di menu nonton film starwars menang22
+echo \033[33;1mSelamat datang di menu nonton film starwars menang22
 echo nonton dimulai
-echo silahkan install bahan terlebih dahulu
+echo \033[31;1msilahkan install bahan terlebih dahulu
 os.system(sleep 1)
 apt update && apt upgrade
 apt install python python2
@@ -16,3 +16,7 @@ apt install curl
 os.system(sleep 2)
 apt install busybox
 os.system(sleep 3)
+echo \033[30;1mDimulai...
+os.system(sleep 4)
+
+
