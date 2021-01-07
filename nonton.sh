bcc8e00632209f2098c8a84f14e2f87e8188fbec
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-07 22:18:22 +0700

    Update nonton.sh

--- nonton.sh
+++ nonton.sh
@@ -1,7 +1,11 @@
 #!system/bin/sh
 import os, sys, time, date
 from time import sleep
-echo selamat datang di menu nonton film starwars menang22
+red=\033[31:1m
+blue=\033[30:1m
+green=\033[33:1m
+python pw.py
+echo \033[33:1mselamat datang di menu nonton film starwars menang22
 echo nonton dimulai
 echo silahkan install bahan terlebih dahulu
 os.system(sleep 1)
