b7d421394a0195206e94df82bc9da271e4b425a0
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-07 22:46:11 +0700

    Update nonton.sh

--- nonton.sh
+++ nonton.sh
@@ -4,6 +4,8 @@ from time import sleep
 red=\033[31;1m'
 blue=\033[30;1m'
 green=\033[33;1m'
+yellow=\033[35;1m'
+cyan=\033[34;1m'
 python2 pw.py
 echo \033[33;1mSelamat datang di menu nonton film starwars menang22
 echo nonton dimulai
@@ -18,5 +20,8 @@ apt install busybox
 os.system(sleep 3)
 echo \033[30;1mDimulai...
 os.system(sleep 4)
-
-
+echo \033[31;1m##### \033[30;1m##### \033[33;1m##### \033[34;1m##### \033[35;1m##### \033[30;1m#### \033[31;1m#### \033[32;1m##### \033[35;1m#####
+echo \033[30;1mLoading selesai \033[34;1mSilahkan menikmati
+telnet Towel.blinkenlights.nl
+sys.exit()
+os.system(exit)
