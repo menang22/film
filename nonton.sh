4f790445f22824226849b3ace3d888f6a1c67513
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-09 11:00:19 +0700

    Update nonton.sh

--- nonton.sh
+++ nonton.sh
@@ -1,4 +1,6 @@
+clear
 #!system/bin/sh
+#nonton dulu dong
 import os, sys, time, date
 from time import sleep
 red=\033[31;1m'
@@ -7,11 +9,12 @@ green=\033[33;1m'
 yellow=\033[35;1m'
 cyan=\033[34;1m'
 python2 pw.py
+os.system('clear') 
 toilet -f standard -F gay "NONTON FILM"
 echo \033[33;1mSelamat datang di menu nonton film starwars menang22
 echo nonton dimulai
 echo \033[31;1msilahkan install bahan terlebih dahulu
-os.system(sleep 1)
+os.system('sleep 1')
 apt update && apt upgrade
 apt install python python2
 apt install git
@@ -26,15 +29,15 @@ gem install lolcat
 gem install bundler
 apt install toilet
 apt install figlet
-os.system(clear)
-os.system(sleep 2)
+os.system('clear')
+os.system('sleep 2')
 apt install busybox
-os.system(clear)
-os.system(sleep 3)
+os.system('clear')
+os.system('sleep 3')
 echo \033[30;1mDimulai...
-os.system(sleep 4)
+os.system('sleep 4')
 echo \033[31;1m##### \033[30;1m##### \033[33;1m##### \033[34;1m##### \033[35;1m##### \033[30;1m#### \033[31;1m#### \033[32;1m##### \033[35;1m#####
 echo \033[30;1mLoading selesai \033[34;1mSilahkan menikmati
 telnet Towel.blinkenlights.nl
 sys.exit()
-os.system(exit)
+os.system('exit')
